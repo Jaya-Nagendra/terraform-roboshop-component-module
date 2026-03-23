c5d90b99558436423884056c47834a1c69fe8f2b
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-23 13:05:50 +0530

    roboshop-component-module

--- main.tf
+++ main.tf
@@ -119,12 +119,12 @@ resource "aws_launch_template" "main" {
 
 resource "aws_autoscaling_group" "main" {
   name                      = "${var.project}-${var.environment}-${var.component}"
-  max_size                  = 1
+  max_size                  = 10
   min_size                  = 1
-  health_check_grace_period = 300
+  health_check_grace_period = 120
   health_check_type         = "ELB"
   desired_capacity          = 1
-  force_delete              = true 
+  force_delete              = false 
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
