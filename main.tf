55e4c674dc930cf5ad9e60e34c35a80a29a1ac05
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-23 12:36:02 +0530

    roboshop-component-module

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_autoscaling_group" "main" {
   name                      = "${var.project}-${var.environment}-${var.component}"
   max_size                  = 1
   min_size                  = 1
-  health_check_grace_period = 120
+  health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = true 
